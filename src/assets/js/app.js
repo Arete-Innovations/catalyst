f47fe6506a1b07e061242fe638f0e644b9914c11
ZmoleCristian <tragdate@gmail.com>
2025-05-13 04:33:31 +0300

    csrf temp fix - fixes #16

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-
+document.addEventListener('DOMContentLoaded', function() {
+	const csrfInput = document.querySelector('input[name="authenticity_token"]');
+	if (!csrfInput || !csrfInput.value) {
+		const hasRefreshed = sessionStorage.getItem('csrfRefreshed');
+		if (!hasRefreshed) {
+			sessionStorage.setItem('csrfRefreshed', 'true');
+			location.reload();
+		}
+	}
 });
