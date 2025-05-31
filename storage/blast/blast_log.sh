ecb31a4a1b5d73b892e35bc09a6cd834877261c3
ZmoleCristian <tragdate@gmail.com>
2025-05-31 07:39:07 +0300

    sanitized dashboard

--- storage/blast/blast_log.sh
+++ storage/blast/blast_log.sh
@@ -1,24 +1,58 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
+RED='\033[1;31m'
+GREEN='\033[1;32m'
 LIGHT_GREEN='\033[1;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-MAGENTA='\033[0;35m'
-CYAN='\033[0;36m'
-NC='\033[0m' # No Color
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+MAGENTA='\033[1;35m'
+CYAN='\033[1;36m'
+WHITE='\033[1;37m'
+NC='\033[0m'
 
 MARKER=0
 
+format_duration() {
+  local seconds="$1"
+  
+  local ns=$(echo "$seconds * 1000000000" | bc | cut -d'.' -f1)
+  local us=$(echo "$seconds * 1000000" | bc | cut -d'.' -f1) 
+  local ms=$(echo "$seconds * 1000" | bc | cut -d'.' -f1)
+  
+  if (( ns < 1000 )); then
+    echo "${ns}ns"
+  elif (( us < 1000 )); then
+    echo "${us}µs"
+  elif (( ms < 1000 )); then
+    echo "${ms}ms"
+  else
+    printf "%.2fs\n" "$seconds"
+  fi
+}
 
 
 process_line() {
   local line="$1"
-  if [[ $line =~ ([\]\[ 0-9.:-]+)\ \[([A-Z]{2,9})\]\ (.*) ]]; then
+  if [[ $line =~ ([0-9]+\-[0-9]{4}-[0-9]{2}-[0-9]{2}\ [0-9]{2}:[0-9]{2}:[0-9]{2})\ \[([A-Z]+)\]\ (.*) ]]; then
     local time=${BASH_REMATCH[1]}
     local level=${BASH_REMATCH[2]}
     local message=${BASH_REMATCH[3]}
+    
+    if [[ "$level" == "TRACE" ]]; then
+      local TIMED=$(echo $time | grep -Po "^[0-9]+\.[0-9]+")
+      if [ $MARKER = 0 ]; then
+        MARKER=$TIMED
+      else
+        local diff=$(echo "$TIMED - $MARKER" | bc)
+        echo "Trace complete: $(format_duration "$diff")"
+        MARKER=0
+      fi
+      local time_only=$(echo "$time" | grep -o '[0-9][0-9]:[0-9][0-9]:[0-9][0-9]')
+      echo -e "$time_only [${MAGENTA}TRACE${NC}] $message"
+      return
+    fi
+    
+    local time_only=$(echo "$time" | grep -o '[0-9][0-9]:[0-9][0-9]:[0-9][0-9]')
     
     local color=$NC
     case $level in
@@ -32,24 +66,17 @@ process_line() {
         color=$YELLOW
         ;;
       "INFO")
-        color=$LIGHT_GREEN
+        color=$WHITE
         ;;
       "DEBUG")
         color=$CYAN
         ;;
-      "TRACE")
-        color=$MAGENTA
-        local TIMED=$(echo $time | grep -Po "^[\d.]+")
-        if [ $MARKER = 0 ]; then
-          MARKER=$TIMED
-        else
-          echo "Trace complete: $(calc $TIMED-$MARKER)s"
-          MARKER=0
-        fi
+      "VOICE"|"SPEECH"|"VOIP")
+        color=$BLUE
         ;;
     esac
     
-    echo -e "$time [${color}$level${NC}] $message"
+    echo -e "$time_only [${color}$level${NC}] $message"
     
   else
     echo "$line"
@@ -60,8 +87,10 @@ LOG_FILE="$1"
 NUM_LINES="200"
 
 if [ ! -f "$LOG_FILE" ]; then
-  echo "Error: Log file $LOG_FILE does not exist."
-  exit 1
+  echo "Log file $LOG_FILE does not exist. Creating it..."
+  mkdir -p "$(dirname "$LOG_FILE")"
+  touch "$LOG_FILE"
+  echo "Created $LOG_FILE"
 fi
 
 echo "=== Showing history of $NUM_LINES lines of $LOG_FILE ==="
